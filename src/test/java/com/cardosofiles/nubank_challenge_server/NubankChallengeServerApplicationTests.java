5d5959355b8d789decb3d382f1b106c3d1cb1fe9
cardosofiles <cardosofiles@outlook.com>
2025-11-20 18:33:38 -0300

    feat: Implementing Implementing the initial Spring Boot configurations and creating the PostgreSQL database in a Docker container.

---
+++ src/test/java/com/cardosofiles/nubank_challenge_server/NubankChallengeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardosofiles.nubank_challenge_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubankChallengeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
