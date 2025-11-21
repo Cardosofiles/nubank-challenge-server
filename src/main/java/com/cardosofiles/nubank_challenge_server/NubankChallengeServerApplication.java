12a0790f27c81ea3dd846fb742f18fafe9b0d808
cardosofiles <cardosofiles@outlook.com>
2025-11-20 22:35:37 -0300

    feat: configuring the application's CORS

--- src/main/java/com/cardosofiles/nubank_challenge_server/NubankChallengeServerApplication.java
+++ src/main/java/com/cardosofiles/nubank_challenge_server/NubankChallengeServerApplication.java
@@ -2,8 +2,12 @@ package com.cardosofiles.nubank_challenge_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.cardosofiles.nubank_challenge_server.config.CorsProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
+
 public class NubankChallengeServerApplication {
 
 	public static void main(String[] args) {
